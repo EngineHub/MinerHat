d51d2b3e987d4f729ed5a3eb82d114cfaeea4866
Eric Stokes <fernferret@gmail.com>
2012-01-31 21:36:37 -0700

    Turns out the keep alive packets are important... Woo 1.1

--- src/main/java/com/sk89q/minerhat/packets/Packet0KeepAlive.java
+++ src/main/java/com/sk89q/minerhat/packets/Packet0KeepAlive.java
@@ -2,20 +2,25 @@ package com.sk89q.minerhat.packets;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class Packet0KeepAlive extends Packet {
 
-    public Packet0KeepAlive() {}
-    
-    @Override
-    public void read(DataInputStream stream) {}
+    public int keepAlive;
 
     @Override
-    public void write(DataOutputStream stream) {}
+    public void read(DataInputStream stream) throws IOException {
+        this.keepAlive = stream.readInt();
+    }
+
+    @Override
+    public void write(DataOutputStream stream) throws IOException {
+        stream.writeInt(this.keepAlive);
+    }
 
     @Override
     public int length() {
-        return 0;
+        return 4;
     }
 
     @Override
