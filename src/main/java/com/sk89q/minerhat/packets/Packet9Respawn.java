15eea011e5a29dc66beb61b6a143fd42019d3e06
Eric Stokes <fernferret@gmail.com>
2012-01-31 20:21:11 -0700

    Update Respawn Packet

--- src/main/java/com/sk89q/minerhat/packets/Packet9Respawn.java
+++ src/main/java/com/sk89q/minerhat/packets/Packet9Respawn.java
@@ -7,22 +7,38 @@ import java.io.IOException;
 public class Packet9Respawn extends Packet {
 
     public byte dimension;
-    
+    public byte difficulty;
+    public byte gameMode;
+    public int height;
+    public long seed;
+    public String levelType;
+
     public Packet9Respawn() {}
 
     @Override
     public void read(DataInputStream stream) throws IOException {
         this.dimension = stream.readByte();
+        this.difficulty = stream.readByte();
+        this.gameMode = stream.readByte();
+        this.height = stream.readShort();
+        this.seed = stream.readLong();
+        this.levelType = read(stream, 16);
+
     }
 
     @Override
     public void write(DataOutputStream stream) throws IOException {
         stream.writeByte(this.dimension);
+        stream.writeByte(this.difficulty);
+        stream.writeByte(this.gameMode);
+        stream.writeShort(this.height);
+        stream.writeLong(this.seed);
+        write(this.levelType, stream);
     }
 
     @Override
     public int length() {
-        return 1;
+        return 13 + this.levelType.length();
     }
 
     @Override
@@ -36,6 +52,8 @@ public class Packet9Respawn extends Packet {
         result.append(this.getClass().getName() + " -> ");
         result.append(" Dimension: ");
         result.append(dimension);
+        result.append(" Type: ");
+        result.append(this.levelType);
         return result.toString();
     }
 }
